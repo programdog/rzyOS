324c5d099bac66cb473da4724f14d4b97fba5ed6
renzhongyu <531353008@qq.com>
2019-10-14 09:11:21 +0800

    1.add task switch function .

--- SRC/main.c
+++ SRC/main.c
@@ -1,39 +1,50 @@
 #include "rzyOS.h"
 
-#define NVIC_INT_CTRL	0xe000ed04
-#define	NVIC_PENDSVSET	0X10000000
-#define	NVIC_SYSPRI2	0xe000ed22
-#define	NVIC_PENDSV_PRI	0x000000ff
-
-#define MEM32(addr)	*(volatile unsigned long *)(addr)
-#define MEM8(addr)	*(volatile unsigned char *)(addr)
-
-void triggerPendSV(void)
-{
-	MEM8(NVIC_SYSPRI2) = NVIC_PENDSV_PRI;
-	MEM32(NVIC_INT_CTRL) = NVIC_PENDSVSET;
-}
+tTask *currentTask;
+tTask *nextTask;
+tTask *taskTable[2];
 
 void tTaskInit(tTask *task, void (*entry)(void *), void *param, tTaskStack *stack)
 {
+	*(--stack) = (unsigned long)(1 << 24);
+	*(--stack) = (unsigned long)entry;
+	*(--stack) = (unsigned long)0x14;
+	*(--stack) = (unsigned long)0x12;
+	*(--stack) = (unsigned long)0x03;
+	*(--stack) = (unsigned long)0x02;
+	*(--stack) = (unsigned long)0x01;
+	*(--stack) = (unsigned long)param;
+	
+	*(--stack) = (unsigned long)0x11;
+	*(--stack) = (unsigned long)0x10;
+	*(--stack) = (unsigned long)0x09;
+	*(--stack) = (unsigned long)0x08;
+	*(--stack) = (unsigned long)0x07;
+	*(--stack) = (unsigned long)0x06;
+	*(--stack) = (unsigned long)0x05;
+	*(--stack) = (unsigned long)0x04;
+	
 	task -> stack = stack;
 }
 
-typedef struct Blocktype_t
+void tTaskSched()
 {
-	unsigned long *stackptr;
-} Blocktype_t;
-
-Blocktype_t *blockPtr; 
+	if (currentTask == taskTable[0])
+	{
+		nextTask = taskTable[1];
+	}
+	else
+	{
+		nextTask = taskTable[0];
+	}
+	
+	tTaskSwitch();
+}
 
 void delay(int count)
 {
 	while(--count > 0);
 }
-
-int flag;
-unsigned long stack_bufffer[1024];
-Blocktype_t block;
 
 tTask tTask1;
 tTask tTask2;
@@ -41,36 +52,41 @@ tTask tTask2;
 tTaskStack task1Env[1024];
 tTaskStack task2Env[1024];
 
-void task1(void *param)
+int task1Flag;
+void task1Entry(void *param)
 {
 	for (;;)
 	{
-		
+		task1Flag = 0;
+		delay(100);
+		task1Flag = 1;
+		delay(100);
 	}
 }
 
-void task2(void *param)
+int task2Flag;
+void task2Entry(void *param)
 {
 	for (;;)
 	{
-		
+		task2Flag = 0;
+		delay(100);
+		task2Flag = 1;
+		delay(100);
 	}
 }
 
 int main()
 {
-	tTaskInit(&tTask1, task1, (void *)0x11111111, &task2Env[1023]);
-	tTaskInit(&tTask1, task1, (void *)0x22222222, &task2Env[1023]);
+	tTaskInit(&tTask1, task1Entry, (void *)0x11111111, &task2Env[1023]);
+	tTaskInit(&tTask1, task1Entry, (void *)0x22222222, &task2Env[1023]);
 	
-	block.stackptr = &stack_bufffer[1024];
-	blockPtr = &block;
-	for (;;)
-	{
-		flag = 0;
-		delay(100);
-		flag = 1;
-		delay(100);
-		
-//		triggerPendSV();
-	}
+	taskTable[0] = &tTask1;
+	taskTable[1] = &tTask2;
+	
+	nextTask = taskTable[0];
+	
+	tTaskRunFirst();
+	
+	return 0;
 }
--- SRC/switch.c
+++ SRC/switch.c
@@ -1,20 +1,45 @@
+#include "rzyOS.h"
+#include "ARMCM3.h"
+
+#define NVIC_INT_CTRL	0xe000ed04
+#define	NVIC_PENDSVSET	0X10000000
+#define	NVIC_SYSPRI2	0xe000ed22
+#define	NVIC_PENDSV_PRI	0x000000ff
+
+#define MEM32(addr)	*(volatile unsigned long *)(addr)
+#define MEM8(addr)	*(volatile unsigned char *)(addr)
+
 __asm void PendSV_Handler(void)
 {
-	IMPORT blockPtr;
-	LDR R0, =blockPtr
-	LDR R0, [R0]
-	LDR R0, [R0]
+	IMPORT currentTask
+	IMPORT nextTask
 	
-	STMDB R0!, {R4-R11} // STMDB倒序压{R4-R11}, !表示把最后的值回写到R0
+	MRS R0, PSP
+	CBZ R0, PendSVHander_nosave
+	NOP
 	
-	LDR R1, =blockPtr
-	LDR R1, [R1]
-	STR R0, [R1]
+PendSVHander_nosave
+	LDR R0, =currentTask
+	LDR R1, =nextTask
+	LDR R2, [R1]
+	STR R2, [R0]
 	
-	ADD R4, R4, #1
-	ADD R5, R5, #1
+	LDR R0, [R2]
+	LDMIA R0!, {R4-R11}
 	
-	LDMIA R0!, {R4-R11}  // LDMIA正序弹{R4-R11}
-		
+	MSR PSP, R0
+	ORR LR, LR, #0X04
 	BX LR
+}
+
+void tTaskRunFirst(void)
+{
+	__set_PSP(0);
+	
+	MEM8(NVIC_SYSPRI2) = NVIC_PENDSV_PRI;
+	MEM32(NVIC_INT_CTRL) = NVIC_PENDSVSET;
+}
+void tTaskSwitch(void)
+{
+	MEM32(NVIC_INT_CTRL) = NVIC_PENDSVSET;
 }
