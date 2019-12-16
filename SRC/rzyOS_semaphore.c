24ee089e2de4eb38d824b4ba40d34786336828eb
renzhongyu <531353008@qq.com.com>
2019-12-16 17:56:06 +0800

    1.supplement .md .

--- SRC/rzyOS_semaphore.c
+++ SRC/rzyOS_semaphore.c
@@ -6,6 +6,7 @@ void rzyOS_sem_init(rzyOS_sem_s *sem, uint32_t start_count, uint32_t max_count)
 	
 	sem -> max_count = max_count;
 	
+	//不限制最大信号值
 	if (NOLIMITE_MAX_COUNT == max_count)
 	{
 		sem -> count = start_count;
--- SRC/rzyOS_semaphore.h
+++ SRC/rzyOS_semaphore.h
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include "rzyOS_event.h"
 
+//不限制最大计数信号值
 #define NOLIMITE_MAX_COUNT 0
 
 typedef struct rzyOS_sem_s
@@ -11,9 +12,11 @@ typedef struct rzyOS_sem_s
 	rzyOS_ecb_s rzyOS_ecb;
 	//计数值
 	uint32_t count;
+	//最大支持计数值
 	uint32_t max_count;
 } rzyOS_sem_s;
 
+//信号初始化
 void rzyOS_sem_init(rzyOS_sem_s *sem, uint32_t start_count, uint32_t max_count);
 
 #endif
