910a6b2be0742a4245d4eb81aac7f22ca9c9ad7c
renzhongyu <531353008@qq.com>
2020-01-08 09:05:06 +0800

    1.add note for rzyOS_mbox.c .

--- SRC/rzyOS_mbox.c
+++ SRC/rzyOS_mbox.c
@@ -1,5 +1,10 @@
 #include "rzyOS_mbox.h"
 
+//邮箱初始化
+//parameter
+//rzyOS_mbox_s *rzyOS_mbox : 要使用的邮箱结构
+//void **msg_buffer ：邮箱buffer地址
+//uint32_t max_count ：邮箱最大空间
 void rzyOS_mbox_init(rzyOS_mbox_s *rzyOS_mbox, void **msg_buffer, uint32_t max_count)
 {
 	rzyOS_event_init(&(rzyOS_mbox -> rzyOS_ecb), event_type_mbox);
--- SRC/rzyOS_mbox.h
+++ SRC/rzyOS_mbox.h
@@ -7,13 +7,19 @@ typedef struct rzyOS_mbox_s
 {
 	//事件控制块
 	rzyOS_ecb_s rzyOS_ecb;
+	//邮箱计数
 	uint32_t count;
+	//读的位置
 	uint32_t read;
+	//写的位置
 	uint32_t write;
+	//最大空间
 	uint32_t max_count;
+	//buffer地址
 	void **msg_buffer;
 } rzyOS_mbox_s;
 
+//邮箱初始化
 void rzyOS_mbox_init(rzyOS_mbox_s *rzyOS_mbox, void **msg_buffer, uint32_t max_count);
 
 #endif
