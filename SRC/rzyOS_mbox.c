b093510f0f36f9d52104f30350b81c01825e10d3
renzhongyu <531353008@qq.com>
2020-01-14 19:48:45 +0800

    1.add note for rzyOS_mbox.c .

--- SRC/rzyOS_mbox.c
+++ SRC/rzyOS_mbox.c
@@ -7,6 +7,7 @@
 //uint32_t max_count ：邮箱最大空间
 void rzyOS_mbox_init(rzyOS_mbox_s *rzyOS_mbox, void **msg_buffer, uint32_t max_count)
 {
+	//事件结构初始化
 	rzyOS_event_init(&(rzyOS_mbox -> rzyOS_ecb), event_type_mbox);
 	
 	rzyOS_mbox -> msg_buffer = msg_buffer;
@@ -16,15 +17,21 @@ void rzyOS_mbox_init(rzyOS_mbox_s *rzyOS_mbox, void **msg_buffer, uint32_t max_c
 	rzyOS_mbox -> write = 0;
 }
 
+//消息等待函数（阻塞模式）
 uint32_t rzyOS_mbox_wait(rzyOS_mbox_s *rzyOS_mbox, void **msg, uint32_t wait_tick)
 {
 	uint32_t status = task_enter_critical();
 	
+	//缓冲区中是否有消息
 	if (rzyOS_mbox -> count > 0)
 	{
+		//如果 > 0 ， 取出消息
 		rzyOS_mbox -> count --;
+		//取出消息指针
 		*msg = rzyOS_mbox -> msg_buffer[rzyOS_mbox -> read];
+		//读索引向后移动
 		rzyOS_mbox -> read ++;
+
 		if (rzyOS_mbox -> read >= rzyOS_mbox -> max_count)
 		{
 			rzyOS_mbox -> read = 0;
@@ -32,19 +39,24 @@ uint32_t rzyOS_mbox_wait(rzyOS_mbox_s *rzyOS_mbox, void **msg, uint32_t wait_tic
 		task_exit_critical(status);
 		return error_no_error;
 	}
+	//缓冲区中无消息
 	else
 	{
+		//把任务插入到任务等待队列
 		rzyOS_event_wait(&(rzyOS_mbox -> rzyOS_ecb), currentTask, (void *)0, event_type_mbox, wait_tick);
 		task_exit_critical(status);
 		
+		//调度切换
 		task_schedule();
 		
+		//从event_msg中获取消息
 		*msg = currentTask -> event_msg;
 		
 		return currentTask -> wait_event_result;
 	}
 }
 
+//消息等待函数（非阻塞模式）
 uint32_t rzyOS_mbox_nowait(rzyOS_mbox_s *rzyOS_mbox, void **msg)
 {
 	uint32_t status = task_enter_critical();
@@ -54,6 +66,7 @@ uint32_t rzyOS_mbox_nowait(rzyOS_mbox_s *rzyOS_mbox, void **msg)
 		rzyOS_mbox -> count --;
 		*msg = rzyOS_mbox -> msg_buffer[rzyOS_mbox -> read];
 		rzyOS_mbox -> read ++;
+		
 		if (rzyOS_mbox -> read >= rzyOS_mbox -> max_count)
 		{
 			rzyOS_mbox -> read = 0;
