e85a186eb9ebaa2744950ee27d901cb2bc1d0bc3
renzhongyu <531353008@qq.com>
2020-02-26 18:37:51 +0800

    1.finish note for rztOS_memory.c .

--- SRC/rzyOS_memory.c
+++ SRC/rzyOS_memory.c
@@ -42,6 +42,10 @@ void rzyOS_mem_block_init(rzyOS_mem_block_s *rzyOS_mem_block, uint8_t *mem_start
 }
 
 //任务获取内存块函数(阻塞)
+//parameter : 
+//rzyOS_mem_block_s *rzyOS_mem_block : 内存管理结构
+//uint8_t **mem : 获取到子内存块地址指针
+//uint32_t wait_time : 超时等待
 uint32_t rzyOS_mem_block_wait(rzyOS_mem_block_s *rzyOS_mem_block, uint8_t **mem, uint32_t wait_time)
 {
 	uint32_t status = task_enter_critical();
@@ -49,6 +53,7 @@ uint32_t rzyOS_mem_block_wait(rzyOS_mem_block_s *rzyOS_mem_block, uint8_t **mem,
 	//统计是否有空闲的内存子块
 	if (list_count(&(rzyOS_mem_block -> block_list)) > 0)
 	{
+		//如果有空闲子内存块, 则从内存块空闲链表中移除
 		*mem = (uint8_t *)remove_list_first(&(rzyOS_mem_block -> block_list));
 		task_exit_critical(status);
 
@@ -56,10 +61,13 @@ uint32_t rzyOS_mem_block_wait(rzyOS_mem_block_s *rzyOS_mem_block, uint8_t **mem,
 	}
 	else
 	{
+		//如果没有空闲子内存块, 则加入内存块等待队列
 		rzyOS_event_wait(&(rzyOS_mem_block -> rzyOS_ecb), currentTask, (void *)0, event_type_mem_block, wait_time);
 		task_exit_critical(status);
 
+		//进行一次调度
 		task_schedule();
+		//获取子内存块地址指针
 		*mem = currentTask -> event_msg;
 
 		return currentTask -> wait_event_result;
@@ -67,6 +75,9 @@ uint32_t rzyOS_mem_block_wait(rzyOS_mem_block_s *rzyOS_mem_block, uint8_t **mem,
 }
 
 //任务获取内存块函数(非阻塞)
+//parameter : 
+//rzyOS_mem_block_s *rzyOS_mem_block : 内存管理结构
+//uint8_t **mem : 获取到子内存块地址指针
 uint32_t rzyOS_mem_block_no_wait(rzyOS_mem_block_s *rzyOS_mem_block, uint8_t **mem)
 {
 	uint32_t status = task_enter_critical();
@@ -88,14 +99,20 @@ uint32_t rzyOS_mem_block_no_wait(rzyOS_mem_block_s *rzyOS_mem_block, uint8_t **m
 }
 
 //子内存块释放函数
+//parameter : 
+//rzyOS_mem_block_s *rzyOS_mem_block : 内存管理结构
+//uint8_t **mem : 释放的子内存块地址指针
 void rzyOS_mem_block_post(rzyOS_mem_block_s *rzyOS_mem_block, uint8_t **mem)
 {
 	uint32_t status = task_enter_critical();
 
+	//统计是否有任务等待内存子块
 	if (rzyOS_event_wait_count(&(rzyOS_mem_block -> rzyOS_ecb)) > 0)
 	{
+		//如果有任务等待,则唤醒,并传递内存子块地址指针
 		task_tcb_s *task_tcb = rzyOS_event_wakeup(&(rzyOS_mem_block -> rzyOS_ecb), (void *)mem, error_no_error);
 
+		//判断被唤醒的任务优先级是否高于当前任务
 		if (task_tcb -> prio < currentTask -> prio)
 		{
 			task_schedule();
@@ -103,6 +120,7 @@ void rzyOS_mem_block_post(rzyOS_mem_block_s *rzyOS_mem_block, uint8_t **mem)
 	}
 	else
 	{
+		//如果没有任务等待,则加入内存块空闲链表中
 		list_add_last(&(rzyOS_mem_block -> block_list), (node_t *)mem);
 	}
 
@@ -114,13 +132,14 @@ uint32_t rzyOS_mem_block_destroy(rzyOS_mem_block_s *rzyOS_mem_block)
 {
 	uint32_t status = task_enter_critical();
 
+	//删除等待空闲块的任务, 并在rzyOS_event_remove_all()把任务插入到就绪队列
 	uint32_t count = rzyOS_event_remove_all(&(rzyOS_mem_block -> rzyOS_ecb), (void *)0, error_delete);
 
 	task_exit_critical(status);
 
+	//若获取移除的个数大于0, 则进行一次调度， 看看是否有更高优先级的任务处于刚才的等待状态
 	if (count > 0)
 	{
-		//若获取移除的个数大于0, 则进行一次调度， 看看是否有更高优先级的任务处于刚才的等待状态
 		task_schedule();
 	}
 
