c2c4b32c30956a9db3f2c4e8078eea1831ed3bd6
renzhongyu <531353008@qq.com>
2020-03-06 18:50:46 +0800

    1.summary up the type of flag group .

--- SRC/rzyOS_flag_group.c
+++ SRC/rzyOS_flag_group.c
@@ -1,5 +1,9 @@
 #include "rzyOS_flag_group.h"
 
+//时间组初始化函数
+//parameter : 
+//rzyOS_flag_group_s *rzyOS_flag_group : 事件标志组
+//uint32_t flags : 标志组类型
 void rzyOS_flag_group_init(rzyOS_flag_group_s *rzyOS_flag_group, uint32_t flags)
 {
 	rzyOS_event_init(&(rzyOS_flag_group -> rzyOS_ecb), event_type_flag_group);
--- SRC/rzyOS_flag_group.h
+++ SRC/rzyOS_flag_group.h
@@ -9,28 +9,33 @@ typedef struct rzyOS_flag_group_s
 	uint32_t flag;
 } rzyOS_flag_group_s;
 
-//任务希望请求的事件标志组时间类型
-//清除
+//标志位形式
+//清除标志位
 #define FLAGGROUP_CLEAR (0x0 << 0)
-//设置
+//设置标志位
 #define FLAGGROUP_SET (0x1 << 0)
-//等待所有的标志位设置后的清零
+//任意的标志位
 #define FLAGGROUP_ANY (0x0 << 1)
-//所有的标志位设置后的清零
+//所有的标志位
 #define FLAGGROUP_ALL (0x1 << 1)
 
-//设置相关
-//所有的标志位都设置
+//标志组合
+//设置所有的标志位
 #define FLAGGROUP_SET_ALL (FLAGGROUP_SET | FLAGGROUP_ALL)
-//任意的标志位都置1
+//设置任意的标志位
 #define FLAGGROUP_SET_ANY (FLAGGROUP_SET | FLAGGROUP_ANY)
-//清除相关
+//清除所有的标志位
 #define FLAGGROUP_CLEAR_ALL (FLAGGROUP_CLEAR | FLAGGROUP_ALL)
+//清除任意的标志位
 #define FLAGGROUP_CLEAR_ANY (FLAGGROUP_CLEAR | FLAGGROUP_ANY)
 
-//是否需要清零或置1
+//是否需要清零或置位
 #define FLAGGROUP_CONSUME (1 << 7)
 
+
+//时间组初始化函数
 void rzyOS_flag_group_init(rzyOS_flag_group_s *rzyOS_flag_group, uint32_t flags);
+
+void rzyOS_flag_group_wait(rzyOS_flag_group_s *rzyOS_flag_group, uint32_t wait_type, uint32_t request_flag, uint32_t *result_flag, uint32_t wait_ticks);
 
 #endif
