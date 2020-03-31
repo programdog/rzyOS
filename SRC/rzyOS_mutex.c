c0e733f3bbe7f750a426862a3047fb49e110c21d
renzhongyu <531353008@qq.com>
2020-03-31 18:52:43 +0800

    1.add note for function rzyOS_mutex_init() 2 .

--- SRC/rzyOS_mutex.c
+++ SRC/rzyOS_mutex.c
@@ -4,8 +4,10 @@
 //互斥锁初始化函数
 void rzyOS_mutex_init(rzyOS_mutex_s *rzyOS_mutex)
 {
+	//互斥锁事件结构初始化
 	rzyOS_event_init(&(rzyOS_mutex -> rzyOS_ecb), event_type_mutex);
 
+	//互斥锁事件结构字段初始化
 	rzyOS_mutex -> lock_count = 0;
 	rzyOS_mutex -> owner = (task_tcb_s *)0;
 	rzyOS_mutex -> owner_original_prio = RZYOS_PRIO_COUNT;
