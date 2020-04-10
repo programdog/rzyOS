fde2cb7ee0c3f9bf86e7eae07777df14db62c12e
renzhongyu <531353008@qq.com>
2020-04-10 09:18:30 +0800

    1.add work queue init function .

--- SRC/rzyOS_work_queue.c
+++ SRC/rzyOS_work_queue.c
@@ -1,2 +1,22 @@
 #include "rzyOS_work_queue.h"
 
+void rzyOS_queue_init(rzyOS_wqueue_s *rzyOS_wqueue, uint32_t start_delay_tick, uint32_t period_tick, worker_t worker, void *arg, uint32_t wqueue_config)
+{
+	node_init(&(rzyOS_wqueue -> node));
+	rzyOS_wqueue -> start_delay_tick = start_delay_tick;
+	rzyOS_wqueue -> period_tick = period_tick;
+
+	if (0 == start_delay_tick)
+	{
+		rzyOS_wqueue -> count_tick = period_tick;
+	}
+	else
+	{
+		rzyOS_wqueue -> count_tick = start_delay_tick;
+	}
+	
+	rzyOS_wqueue -> worker = worker;
+	rzyOS_wqueue -> arg = arg;
+	rzyOS_wqueue -> wqueue_config = wqueue_config;
+	rzyOS_wqueue -> rzyOS_wqueue_status = wqueue_create;
+}
--- SRC/rzyOS_work_queue.h
+++ SRC/rzyOS_work_queue.h
@@ -6,28 +6,30 @@
 //Defines the work callback
 typedef void (*worker_t)(void *arg);
 
-typedef enum rzyOS_queue_status_e
+typedef enum rzyOS_wqueue_status_e
 {
 	wqueue_create,
 	wqueue_start,
 	wqueue_running,
 	wqueue_stop,
 	wqueue_destroy,
-} rzyOS_queue_status_e;
+} rzyOS_wqueue_status_e;
 
 typedef struct rzyOS_wqueue_s
 {
 	node_t node;
 	uint32_t start_delay_tick;
 	uint32_t period_tick;
-	uint32_t delay_count_tick;
+	uint32_t count_tick;
 	worker_t worker;
 	void *arg;
 	uint32_t wqueue_config;
-	rzyOS_queue_status_e rzyOS_queue_status;
+	rzyOS_wqueue_status_e rzyOS_wqueue_status;
 } rzyOS_wqueue_s;
 
 #define HIGH_WORK_QUEUE 1
 #define LOW_WORK_QUEUE 0
+
+void rzyOS_queue_init(rzyOS_wqueue_s *rzyOS_wqueue, uint32_t start_delay_tick, uint32_t period_tick, worker_t worker, void *arg, uint32_t wqueue_config);
 
 #endif
