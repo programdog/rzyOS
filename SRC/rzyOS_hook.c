61d824d4ca86dbd6d9d5bb9f99f3092a24846539
renzhongyu <531353008@qq.com>
2020-05-10 23:41:48 +0800

    1.add rzyOS core hook function .

---
+++ SRC/rzyOS_hook.c
@@ -0,0 +1,16 @@
+#include "rzyOS_hook.h"
+
+void rzyOS_hook_idle(void)
+{
+
+}
+
+void rzyOS_hook_systick(void)
+{
+
+}
+
+void rzyOS_hook_schedule(task_tcb_s *this_task, task_tcb_s *next_task)
+{
+
+}
---
+++ SRC/rzyOS_hook.h
@@ -0,0 +1,9 @@
+#ifndef RZYOS_HOOK_H
+#define RZYOS_HOOK_H
+
+void rzyOS_hook_idle(void);
+void rzyOS_hook_systick(void);
+void rzyOS_hook_schedule(task_tcb_s *this_task, task_tcb_s *next_task);
+
+
+#endif
