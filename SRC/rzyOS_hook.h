5b03b94783589f40fee2f9bf890de01ca30cf821
renzhongyu <531353008@qq.com>
2020-05-10 23:44:16 +0800

    1.fix a bug in include .h file .

--- SRC/rzyOS_hook.h
+++ SRC/rzyOS_hook.h
@@ -1,6 +1,8 @@
 #ifndef RZYOS_HOOK_H
 #define RZYOS_HOOK_H
 
+#include "rzyOS.h"
+
 void rzyOS_hook_idle(void);
 void rzyOS_hook_systick(void);
 void rzyOS_hook_schedule(task_tcb_s *this_task, task_tcb_s *next_task);
