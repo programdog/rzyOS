8a84a88d2d91cf7b486d3566eba31570d3817531
renzhongyu <531353008@qq.com>
2020-05-16 21:53:21 +0800

    1.add stm32f4-discovery led configuration .

---
+++ board/stm32f4_discovery/LED/led.c
@@ -0,0 +1,21 @@
+#include "led.h" 
+#include "stm32f4xx.h" 
+
+
+//初始化PD12~PD15.并使能GPIOD的时钟
+//LED IO初始化
+void LED_Init(void)
+{
+	GPIO_InitTypeDef  GPIO_InitStructure;
+
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);	//使能GPIOD时钟
+
+	//PD12~PD15初始化设置
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;	//LED对应IO口
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;			//普通输出模式
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;			//推挽输出
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;		//50MHz
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;			//上拉
+	GPIO_Init(GPIOD, &GPIO_InitStructure);					//初始化GPIO
+	GPIO_ResetBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);	//设置0，灯灭
+}
