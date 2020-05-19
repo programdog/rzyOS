64ef6deb4b90b75a75e0795002447ae390168f02
renzhongyu <531353008@qq.com>
2020-05-19 23:13:38 +0800

    1.include rzyOS kernel .h in rzyOS_api.h .

--- rzyOS_kernel/rzyOS_api.h
+++ rzyOS_kernel/rzyOS_api.h
@@ -3,4 +3,13 @@
 
 #include "rzyOS_schedule.h"
 
+#include "rzyOS_eventgroup.h"
+#include "rzyOS_hook.h"
+#include "rzyOS_mbox.h"
+#include "rzyOS_memory.h"
+#include "rzyOS_semaphore.h"
+#include "rzyOS_mutex.h"
+#include "rzyOS_wqueue.h"
+
+
 #endif
