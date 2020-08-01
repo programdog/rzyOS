fcc9ceb8a8f027817d1ca4fed092e5029e11516c
renzhongyu <531353008@qq.com>
2020-08-01 17:17:15 +0800

    1.add device tree insert function .

--- rzyOS_kernel/rzyOS_vfs/rzyOS_vfs.c
+++ rzyOS_kernel/rzyOS_vfs/rzyOS_vfs.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include "rzyOS_mm1.h"
 #include "rzyOS_vfs.h"
 
 
@@ -6,6 +7,8 @@ static vfs_root_mangement_s *vfs_root_mangement = NULL;
 static vfs_root_mangement_s vfs_root_mangement_instance;
 
 static vfs_node_s vfs_node_root;
+
+static int vfs_insert_node_r(vfs_node_s **node, char *abs_path, file_operations_s ops);
 
 
 int vfs_init(void)
@@ -26,4 +29,77 @@ int vfs_init(void)
 	return 0;
 }
 
+
+int vfs_insert_node(char *path, file_operations_s ops)
+{
+	if (path[0] != '/')
+	{
+		return -1;
+	}
+
+	rzyOS_sem_wait(&vfs_root_mangement -> sem_rw, 0);
+	int ret = vfs_insert_node_r(&vfs_root_mangement -> root_node -> child, &path[1], ops);
+	rzyOS_sem_post(&vfs_root_mangement -> sem_rw);
+	return ret;
+}
+
+
+int vfs_insert_node_r(vfs_node_s **node, char *abs_path, file_operations_s ops)
+{
+	if (node == NULL)
+	{
+		return -1;
+	}
+	if (abs_path == NULL)
+	{
+		return -1;
+	}
+	if (abs_path[0] == 0)
+	{
+		return -1;
+	}
+
+	char node_name[NODE_NAME_SIZE] = {0};
+
+	char *p = abs_path;
+
+	for (int i = 0; (*p != '/') && (*p != 0) && (i < NODE_NAME_SIZE); i ++)
+	{
+		node_name[i] = *p++;
+	}
+
+	if (*p == '/' && *p != 0)
+	{
+		p++;
+	}
+
+	while (*node != NULL)
+	{
+		if (strcmp((*node) -> name, node_name) == 0)
+		{
+			return vfs_insert_node_r(&(*node) -> child, p, ops);
+		}
+		node = &(*node) -> brother;
+	}
+
+	vfs_node_s *node_new = malloc(sizeof(vfs_node_s));
+
+	strncpy(node_new -> name, node_name, NODE_NAME_SIZE);
+
+	node_new -> child = NULL;
+	node_new -> brother = NULL;
+
+	memset(&node_new -> ops, 0, sizeof(file_operations_s));
+
+	*node = node_new;
+
+	if (*p != 0)
+	{
+		return vfs_insert_node_r(&node_new -> child, p, ops);
+	}
+
+	memcpy(&node_new -> ops, &ops, sizeof(file_operations_s));
+
+	return 0;
+}
 
