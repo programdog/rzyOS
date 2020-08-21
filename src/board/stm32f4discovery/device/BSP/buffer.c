ffc045a2821c8cd6b149cee5338512ff4b274cc1
renzhongyu <531353008@qq.com>
2020-08-21 19:17:46 +0800

    1.add buffer counte function .

--- src/board/stm32f4discovery/device/BSP/buffer.c
+++ src/board/stm32f4discovery/device/BSP/buffer.c
@@ -8,3 +8,18 @@ uint8_t uart3_rx_buffer_available()
 {
 	
 }
+
+
+uint8_t uart3_rx_buffer_used()
+{
+	uint8_t tail_count = uart3_rx_buffer_tail;
+	
+	if (uart3_rx_buffer_head >= tail_count)
+	{
+		return (uart3_rx_buffer_head - tail_count);
+	}
+	else //uart3_rx_buffer_head < tail_count
+	{
+		return (UART3_RX_BUF_SIZE - tail_count + uart3_rx_buffer_head);
+	}
+}
