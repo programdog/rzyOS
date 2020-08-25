de31181e4b4289b076ebbd6c6875c56f5bf0bbc6
renzhongyu <531353008@qq.com>
2020-08-25 13:26:04 +0800

    1.finish uart3 buffer handle .

--- src/board/stm32f4discovery/device/BSP/buffer.c
+++ src/board/stm32f4discovery/device/BSP/buffer.c
@@ -5,15 +5,20 @@ uint8_t uart3_rx_buffer_head = 0;
 uint8_t uart3_rx_buffer_tail = 0;
 
 
+uint8_t uart3_rx_buffer_unused(void);
+uint8_t uart3_rx_buffer_used(void);
+void uart3_rx_buffer_clear(void);
+
+
 //buffer未使用量
-uint8_t uart3_rx_buffer_unused()
+uint8_t uart3_rx_buffer_unused(void)
 {
 	return (UART3_RX_BUF_SIZE - uart3_rx_buffer_used());
 }
 
 
 //buffer使用量
-uint8_t uart3_rx_buffer_used()
+uint8_t uart3_rx_buffer_used(void)
 {
 	uint8_t tail_index = uart3_rx_buffer_tail;
 	
@@ -25,12 +30,17 @@ uint8_t uart3_rx_buffer_used()
 	{
 		return (UART3_RX_BUF_SIZE - tail_index + uart3_rx_buffer_head);
 	}
-	else if (uart3_rx_buffer_head == tail_index)
+	else //uart3_rx_buffer_head == tail_index
 	{
 		return 0;
 	}
 }
 
+void uart3_rx_buffer_clear(void)
+{
+	uart3_rx_buffer_head = 0;
+	uart3_rx_buffer_tail = 0;
+}
 
 int uart3_rx_buffer_read(uint8_t *c)
 {
@@ -50,5 +60,14 @@ int uart3_rx_buffer_read(uint8_t *c)
 
 void uart3_rx_buffer_write(uint8_t c)
 {
-
+	if (uart3_rx_buffer_unused())
+	{
+		uart3_rx_buffer_clear();
+	}
+	else
+	{
+		uart3_rx_buffer[uart3_rx_buffer_head] = c;
+		uart3_rx_buffer_head ++;
+		uart3_rx_buffer_head %= UART3_RX_BUF_SIZE;
+	}
 }
--- src/board/stm32f4discovery/device/BSP/buffer.h
+++ src/board/stm32f4discovery/device/BSP/buffer.h
@@ -5,12 +5,10 @@
 
 #define UART3_RX_BUF_SIZE 256
 
-uint8_t uart3_rx_buffer_unused();
-
-uint8_t uart3_rx_buffer_used();
-
 int uart3_rx_buffer_read(uint8_t *c);
 
 void uart3_rx_buffer_write(uint8_t c);
+
+
 
 #endif
