2bee34d8ecd83447966194172e681af89ac49404
renzhongyu <531353008@qq.com>
2020-11-10 18:28:47 +0800

    1.add terminal cmd struct .

--- rzyOS_kernel/rzyOS_rsh.h
+++ rzyOS_kernel/rzyOS_rsh.h
@@ -3,6 +3,18 @@
 
 #include <stdio.h>
 
+typedef struct rzyOS_cmd_s
+{
+	//命令字符串表
+	char const *cmd_list;
+	//最大支持参数个数
+	uint32_t max_args;
+	//命令回调函数
+	void (*handle)(int argc, void *argv);
+	//命令使用帮助
+	char *help;
+} rzyOS_cmd_s;
+
 void rzyOS_rzh_show(void);
 
 #endif
